7044640cb643f5c4a2b229ed045522cd10e40961
Owen Corso <owen@strattonimaging.com>
2010-04-01 18:54:44 -0400

    got fpo site working

--- js/site/init.js
+++ js/site/init.js
@@ -9,10 +9,10 @@ jQuery(document).ready(function($)
 	if(swfobject.hasFlashPlayerVersion("6.0.65"))
 	{
 		if(!swfobject.hasFlashPlayerVersion(swfEmbed.minimumVersion)) swfEmbed.parameters = {};
-		$("#top").show();
+		//$("#top").show();
 		//disable flash for now!!
-		//$("#top").html("<div id='flash'></div>");
-		//swfobject.embedSWF("loader.swf","flash", "100%", "100%",swfEmbed.minimumVersion,"swf/expressInstall.swf",swfEmbed.flashvars,swfEmbed.parameters,swfEmbed.attributes);
+		$("#top").html("<div id='flash'></div>");
+		swfobject.embedSWF("loader.swf","flash", "100%", "100%",swfEmbed.minimumVersion,"swf/expressInstall.swf",swfEmbed.flashvars,swfEmbed.parameters,swfEmbed.attributes);
 	}
 	else {
 		$("#top").show();
