4cd01b8f77b520c6d60b3a0b86576b5cb1370fa5
Owen Corso <owen@strattonimaging.com>
2010-04-15 10:22:01 -0400

    pre pull commit

--- php/application/controllers/ftpcontroller.php
+++ php/application/controllers/ftpcontroller.php
@@ -58,6 +58,38 @@ class FtpController extends Controller {
 		
 		
 	}//end function
+	
+	/**
+	 * Converts human readable file size (e.g. 10 MB, 200.20 GB) into bytes.
+	 *
+	 * @param string $str
+	 * @return int the result is in bytes
+	 * @author Svetoslav Marinov
+	 * @author http://slavi.biz
+	 */
+	function filesize2bytes($str) {
+	    $bytes = 0;
+
+	    $bytes_array = array(
+	        'B' => 1,
+	        'KB' => 1024,
+	        'MB' => 1024 * 1024,
+	        'GB' => 1024 * 1024 * 1024,
+	        'TB' => 1024 * 1024 * 1024 * 1024,
+	        'PB' => 1024 * 1024 * 1024 * 1024 * 1024,
+	    );
+
+	    $bytes = floatval($str);
+
+	    if (preg_match('#([KMGTP]?B)$#si', $str, $matches) && !empty($bytes_array[$matches[1]])) {
+	        $bytes *= $bytes_array[$matches[1]];
+	    }
+
+	    $bytes = intval(round($bytes, 2));
+
+	    return $bytes;
+	}
+
 }//end class
 
 
